c38789f14fba406d7714ae07c8784d18e0064de7
Siek Chhaihang <chhayhangs@gmail.com>
2025-04-05 17:47:24 +0700

    Make logo dynamic on sign in page

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -13,17 +13,21 @@ export default async function AuthLayout({
     <div className="flex flex-col items-center min-h-screen highlight-link  ">
       <header className="mt-8">
         <Link href="/">
-          <Image
-            src="/icons/logo.svg"
-            alt="logo"
-            width={64}
-            height={64}
-            priority
-            style={{
-              maxWidth: "100%",
-              height: "auto",
-            }}
-          />
+          {site.logo ? (
+            <Image
+              src={site.logo}
+              alt="logo"
+              width={64}
+              height={64}
+              priority
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+              }}
+            />
+          ) : (
+            <span className="text-lg font-semibold">{site.name}</span>
+          )}
         </Link>
       </header>
       <main className="mx-auto max-w-sm min-w-80 p-4">{children}</main>
