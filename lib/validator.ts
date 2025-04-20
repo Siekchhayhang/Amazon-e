2f32d407831faefa1ff6d9f8596b3b70ad648ee4
Siek Chhaihang <chhayhangs@gmail.com>
2025-04-20 11:02:25 +0700

    change reg password from 1 to 8 char

--- lib/validator.ts
+++ lib/validator.ts
@@ -155,7 +155,12 @@ const UserName = z
   .min(2, { message: 'Username must be at least 2 characters' })
   .max(50, { message: 'Username must be at most 30 characters' })
 const Email = z.string().min(1, 'Email is required').email('Email is invalid')
-const Password = z.string().min(3, 'Password must be at least 3 characters')
+const Password = z.string().min(8, "Password must be at least 8 characters")
+  .max(100)
+  .regex(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+  )
 const UserRole = z.string().min(1, 'role is required')
 
 export const UserUpdateSchema = z.object({
