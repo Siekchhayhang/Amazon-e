02ee92f3d171e8a9d0158d15cf494164366eb3fa
Siek Chhaihang <chhayhangs@gmail.com>
2025-06-17 20:31:43 +0700

    add speed insight for vercel into rootlayout

--- app/[locale]/(root)/layout.tsx
+++ app/[locale]/(root)/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default async function RootLayout({
   children,
@@ -11,7 +12,10 @@ export default async function RootLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 flex flex-col p-4">{children}</main>
+      <main className="flex-1 flex flex-col p-4">
+        {children}
+        <SpeedInsights />
+      </main>
       <Footer />
     </div>
   );
