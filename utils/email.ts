fa5291ed8715ebd8eddf6505221fb653bcee3298
Siek Chhaihang <chhayhangs@gmail.com>
2025-07-02 20:08:49 +0700

    change hard code to sender email to dynamic

--- utils/email.ts
+++ utils/email.ts
@@ -10,7 +10,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail(to: string, subject: string, html: string) {
     try {
         const response = await resend.emails.send({
-            from: `${process.env.NEXT_PUBLIC_APP_NAME} <noreply@collectionshop.it.com>`, // Use a verified sender
+            from: `${process.env.NEXT_PUBLIC_APP_NAME} ${process.env.SENDER_EMAIL}`, // Use a verified sender
             to,
             subject,
             html,
