38db5ea81517ffac75fd9a9fac5cee6097160f32
Siek Chhaihang <chhayhangs@gmail.com>
2025-07-03 21:27:28 +0700

    test

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,7 +6,7 @@ export const APP_DESCRIPTION =
     'An Amazon clone built with Next.js and MongoDB'
 
 export const SENDER_NAME = process.env.SENDER_NAME || 'support'
-export const SENDER_EMAIL = process.env.SENDER_EMAIL
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
 
 export const USER_ROLES = ['Admin', 'User']
 export const COLORS = ['Gold', 'Green', 'Red']
