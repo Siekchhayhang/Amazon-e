38079e4e609b73cc2e83e0cd1d336d69c80ca127
Siek Chhaihang <chhayhangs@gmail.com>
2025-07-10 20:37:04 +0700

    testing new generateMetadata function

--- app/[locale]/(root)/product/[slug]/page.tsx
+++ app/[locale]/(root)/product/[slug]/page.tsx
@@ -1,76 +1,107 @@
-import { auth } from '@/auth'
-import AddToCart from '@/components/shared/product/add-to-cart'
-import { Card, CardContent } from '@/components/ui/card'
+import { auth } from "@/auth";
+import AddToCart from "@/components/shared/product/add-to-cart";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   getProductBySlug,
   getRelatedProductsByCategory,
-} from '@/lib/actions/product.actions'
+} from "@/lib/actions/product.actions";
 
-import ReviewList from './review-list'
-import { generateId, round2 } from '@/lib/utils'
-import SelectVariant from '@/components/shared/product/select-variant'
-import ProductPrice from '@/components/shared/product/product-price'
-import ProductGallery from '@/components/shared/product/product-gallery'
-import AddToBrowsingHistory from '@/components/shared/product/add-to-browsing-history'
-import { Separator } from '@/components/ui/separator'
-import BrowsingHistoryList from '@/components/shared/browsing-history-list'
-import RatingSummary from '@/components/shared/product/rating-summary'
-import ProductSlider from '@/components/shared/product/product-slider'
-import { getTranslations } from 'next-intl/server'
+import ReviewList from "./review-list";
+import { generateId, round2 } from "@/lib/utils";
+import SelectVariant from "@/components/shared/product/select-variant";
+import ProductPrice from "@/components/shared/product/product-price";
+import ProductGallery from "@/components/shared/product/product-gallery";
+import AddToBrowsingHistory from "@/components/shared/product/add-to-browsing-history";
+import { Separator } from "@/components/ui/separator";
+import BrowsingHistoryList from "@/components/shared/browsing-history-list";
+import RatingSummary from "@/components/shared/product/rating-summary";
+import ProductSlider from "@/components/shared/product/product-slider";
+import { getTranslations } from "next-intl/server";
+import { NEXT_PUBLIC_SERVER_URL } from "@/lib/constants";
+import { i18n } from "@/i18n-config";
 
 export async function generateMetadata(props: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ slug: string; locale: string }>;
 }) {
-  const t = await getTranslations()
-  const params = await props.params
-  const product = await getProductBySlug(params.slug)
+  const t = await getTranslations();
+  const params = await props.params;
+  const product = await getProductBySlug(params.slug);
+  const baseUrl = NEXT_PUBLIC_SERVER_URL;
   if (!product) {
-    return { title: t('Product.Product not found') }
+    return { title: t("Product.Product not found") };
   }
   return {
     title: product.name,
     description: product.description,
-  }
+    alternates: {
+      canonical: `${baseUrl}/${params.locale}/product/${params.slug}`,
+      languages: Object.fromEntries(
+        i18n.locales.map((locale) => [
+          locale.code,
+          `${baseUrl}/${locale.code}/product/${params.slug}`,
+        ])
+      ),
+    },
+    openGraph: {
+      title: product.name,
+      description: product.description,
+      url: `${baseUrl}/${params.locale}/product/${params.slug}`,
+      images: [
+        {
+          url: product.images[0],
+          width: 800,
+          height: 600,
+          alt: product.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: product.name,
+      description: product.description,
+      images: [product.images[0]],
+    },
+  };
 }
 
 export default async function ProductDetails(props: {
-  params: Promise<{ slug: string }>
-  searchParams: Promise<{ page: string; color: string; size: string }>
+  params: Promise<{ slug: string; locale: string }>;
+  searchParams: Promise<{ page: string; color: string; size: string }>;
 }) {
-  const searchParams = await props.searchParams
+  const searchParams = await props.searchParams;
 
-  const { page, color, size } = searchParams
+  const { page, color, size } = searchParams;
 
-  const params = await props.params
+  const params = await props.params;
 
-  const { slug } = params
+  const { slug } = params;
 
-  const session = await auth()
+  const session = await auth();
 
-  const product = await getProductBySlug(slug)
+  const product = await getProductBySlug(slug);
 
   const relatedProducts = await getRelatedProductsByCategory({
     category: product.category,
     productId: product._id,
-    page: Number(page || '1'),
-  })
+    page: Number(page || "1"),
+  });
 
-  const t = await getTranslations()
+  const t = await getTranslations();
   return (
     <div>
       <AddToBrowsingHistory id={product._id} category={product.category} />
       <section>
-        <div className='grid grid-cols-1 md:grid-cols-5  '>
-          <div className='col-span-2'>
+        <div className="grid grid-cols-1 md:grid-cols-5  ">
+          <div className="col-span-2">
             <ProductGallery images={product.images} />
           </div>
 
-          <div className='flex w-full flex-col gap-2 md:p-5 col-span-2'>
-            <div className='flex flex-col gap-3'>
-              <p className='p-medium-16 rounded-full bg-grey-500/10   text-grey-500'>
-                {t('Product.Brand')} {product.brand} {product.category}
+          <div className="flex w-full flex-col gap-2 md:p-5 col-span-2">
+            <div className="flex flex-col gap-3">
+              <p className="p-medium-16 rounded-full bg-grey-500/10   text-grey-500">
+                {t("Product.Brand")} {product.brand} {product.category}
               </p>
-              <h1 className='font-bold text-lg lg:text-xl'>{product.name}</h1>
+              <h1 className="font-bold text-lg lg:text-xl">{product.name}</h1>
 
               <RatingSummary
                 avgRating={product.avgRating}
@@ -79,12 +110,12 @@ export default async function ProductDetails(props: {
                 ratingDistribution={product.ratingDistribution}
               />
               <Separator />
-              <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
-                <div className='flex gap-3'>
+              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+                <div className="flex gap-3">
                   <ProductPrice
                     price={product.price}
                     listPrice={product.listPrice}
-                    isDeal={product.tags.includes('todays-deal')}
+                    isDeal={product.tags.includes("todays-deal")}
                     forListing={false}
                   />
                 </div>
@@ -97,40 +128,40 @@ export default async function ProductDetails(props: {
                 color={color || product.colors[0]}
               />
             </div>
-            <Separator className='my-2' />
-            <div className='flex flex-col gap-2'>
-              <p className='p-bold-20 text-grey-600'>
-                {t('Product.Description')}:
+            <Separator className="my-2" />
+            <div className="flex flex-col gap-2">
+              <p className="p-bold-20 text-grey-600">
+                {t("Product.Description")}:
               </p>
-              <p className='p-medium-16 lg:p-regular-18'>
+              <p className="p-medium-16 lg:p-regular-18">
                 {product.description}
               </p>
             </div>
           </div>
           <div>
             <Card>
-              <CardContent className='p-4 flex flex-col  gap-4'>
+              <CardContent className="p-4 flex flex-col  gap-4">
                 <ProductPrice price={product.price} />
 
                 {product.countInStock > 0 && product.countInStock <= 3 && (
-                  <div className='text-destructive font-bold'>
-                    {t('Product.Only X left in stock - order soon', {
+                  <div className="text-destructive font-bold">
+                    {t("Product.Only X left in stock - order soon", {
                       count: product.countInStock,
                     })}
                   </div>
                 )}
                 {product.countInStock !== 0 ? (
-                  <div className='text-green-700 text-xl'>
-                    {t('Product.In Stock')}
+                  <div className="text-green-700 text-xl">
+                    {t("Product.In Stock")}
                   </div>
                 ) : (
-                  <div className='text-destructive text-xl'>
-                    {t('Product.Out of Stock')}
+                  <div className="text-destructive text-xl">
+                    {t("Product.Out of Stock")}
                   </div>
                 )}
 
                 {product.countInStock !== 0 && (
-                  <div className='flex justify-center items-center'>
+                  <div className="flex justify-center items-center">
                     <AddToCart
                       item={{
                         clientId: generateId(),
@@ -153,21 +184,21 @@ export default async function ProductDetails(props: {
           </div>
         </div>
       </section>
-      <section className='mt-10'>
-        <h2 className='h2-bold mb-2' id='reviews'>
-          {t('Product.Customer Reviews')}
+      <section className="mt-10">
+        <h2 className="h2-bold mb-2" id="reviews">
+          {t("Product.Customer Reviews")}
         </h2>
         <ReviewList product={product} userId={session?.user.id} />
       </section>
-      <section className='mt-10'>
+      <section className="mt-10">
         <ProductSlider
           products={relatedProducts.data}
-          title={t('Product.Best Sellers in', { name: product.category })}
+          title={t("Product.Best Sellers in", { name: product.category })}
         />
       </section>
       <section>
-        <BrowsingHistoryList className='mt-10' />
+        <BrowsingHistoryList className="mt-10" />
       </section>
     </div>
-  )
+  );
 }
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
 import { getSetting } from "@/lib/actions/setting.actions";
 import { cookies } from "next/headers";
+import { NEXT_PUBLIC_SERVER_URL } from "@/lib/constants";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,17 +20,48 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export async function generateMetadata() {
+// export async function generateMetadata() {
+//   const {
+//     site: { slogan, name, description, url },
+//   } = await getSetting();
+//   return {
+//     title: {
+//       template: `%s | ${name}`,
+//       default: `${name}. ${slogan}`,
+//     },
+//     description: description,
+//     metadataBase: new URL(url),
+//   };
+// }
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}) {
   const {
     site: { slogan, name, description, url },
   } = await getSetting();
+
+  const baseUrl = url ?? NEXT_PUBLIC_SERVER_URL;
+  const locale = params.locale;
+
+  const alternates: Record<string, string> = {};
+  routing.locales.forEach((loc) => {
+    alternates[loc] = `${baseUrl}/${loc}`;
+  });
+
   return {
     title: {
       template: `%s | ${name}`,
       default: `${name}. ${slogan}`,
     },
     description: description,
-    metadataBase: new URL(url),
+    metadataBase: new URL(baseUrl),
+    alternates: {
+      canonical: `${baseUrl}/${locale}`,
+      languages: alternates,
+    },
   };
 }
 
