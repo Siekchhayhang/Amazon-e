bb17eaaa6aecd50bedfb94e07af9d3c87975b68c
Siek Chhaihang <chhayhangs@gmail.com>
2026-01-10 15:27:29 +0700

    change from import Link from next/link to Link from @i18n/routing in footer.tsx file

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ChevronUp } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
+import { Link } from "@/i18n/routing";
 
 import { Button } from "@/components/ui/button";
 import useSettingStore from "@/hooks/use-setting-store";
